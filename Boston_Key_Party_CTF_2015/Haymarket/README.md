94ffe478255bdc7215855f8bd47594413c4014f7
aaronst <aaronst@uw.edu>
2015-03-01 17:22:19 -0800

    Updated files

--- Boston_Key_Party_CTF_2015/Haymarket/README.md
+++ Boston_Key_Party_CTF_2015/Haymarket/README.md
@@ -1,1 +1,13 @@
 # Haymarket
+
+Monty Hall wrote a script of how he was supposed to run one of his game shows for his trusty accounting computer some time ago, but hes not really sure what the punch cards mean any more. I mean, that was a while ago. Only, hes sure his key is hidden somewhere in these punch-cards, if he could figure out how to run them... : 150
+
+http://bostonkeyparty.net/haymarket.tar.gz.fe35f59bfa869a0555e9972efa3ddd2d
+
+This challenge involved reversing punched cards in order to read the program and get the flag.  The following URL was super helpful:
+	- http://www.kloth.net/services/cardpunch.php
+To solve it, I wrote a python script, haymarket.py, which takes a punched card image and outputs the correct characters to a file.
+
+Extracting the given file results in a directory which contained 32 PNG images of punched cards.  Because all of the images followed a strict layout, the exact coordinates of every column and row could be calculated manually with image editting software such as GIMP. Also, the exact RGB values of a "punch" was obtained, which laid the ground-work to write a small image recognition script for this challenge.  Using the URL from above, I was able to see the entire character set and each character's corresponding punch pattern.  I hard-coded this information into my script using lists, and then let it iterate over every column on the punched card.  For every column, my script determines which rows are "punched" by looking at the R(ed) value, and stores the indeces of those rows in a temporary list.  After it has gone over every row, it performs a look-up in my list of punch patters to figure out what character it just read.  Once the character is found, it adds it to a string and moves on to the next column.  My script does this for every row, and then appends the resulting string to a text file, haymarket.txt.  Finding the flag was just a matter of reading the text file after running the script on every image.
+
+Flag = "ALEXTREBEKISASOCIALENGINEER"
--- Boston_Key_Party_CTF_2015/Haymarket/haymarket.py
+++ Boston_Key_Party_CTF_2015/Haymarket/haymarket.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
 
+# Boston Key Party CTF 2015
+# Haymarket
+#
+# Author:  aaronst
+#
+# This script reads in a PNG image of a punched card and outputs the correct characters to
+# a text file.
+#
+# Usage:  $ ./haymarket.py <punched_card.png>
+
 import Image
 import sys
 
@@ -8,18 +18,23 @@ if (len(sys.argv) != 2):
 
 image = Image.open(sys.argv[1])
 
+# Punched card layout values.  Determined by opening up one of the given images in GIMP.
 column_width = 7
 column_height = 20
 first_column_x = 15
 first_column_y = 20
 punch_color = 20
 
+# List of the character set for the punched cards.
 chars = ['&', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C',
 		'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
 		'/', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ':', '#', '@', '\'', '=', '\"',
 		'[', '.', '<', '(', '+', '^', '!', '$', '*', ')', ';', '\\', ']', ',', '%',
 		'_', '>', '?']
 
+# List of the punch patterns corresponding (by index) to the characters in the chars list.  Each number
+# represents the index of a "punched" row, with 0 being the top row and 11 being the bottom.  These
+# patterns were determined using the punched card emulator at http://www.kloth.net/services/cardpunch.php.
 patterns = [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [0, 3], [0, 4],
 			[0, 5], [0, 6], [0, 7], [0, 8], [0, 9], [0, 10], [0, 11], [1, 3], [1, 4], [1, 5],
 			[1, 6], [1, 7], [1, 8], [1, 9], [1, 10], [1, 11], [2, 3], [2, 4], [2, 5], [2, 6],
@@ -28,19 +43,34 @@ patterns = [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [0, 3]
 			[0, 9, 10], [1, 4, 10], [1, 5, 10], [1, 6, 10], [1, 7, 10], [1, 8, 10], [1, 9, 10],
 			[2, 4, 10], [2, 5, 10], [2, 6, 10], [2, 7, 10], [2, 8, 10], [2, 9, 10]]
 
+# Initial string for output.
 text = ""
 
+# For every column on the punched card...
 for i in range (0, 80):
+	
+	# Initial pattern list.
 	pattern = []
+	
+	# For every row in the current column...
 	for j in range (0, 12):
+
+		# If the current row is "punched"...
 		if (image.getpixel((first_column_x + (i * column_width),
 			first_column_y + (j * column_height)))[0] == punch_color):
+			
+			# Record the index in our pattern list.
 			pattern.append(j)
 
+	# Ignore empty columns.
 	if (pattern != []):
+		
+		# Record the character that corresponds with the resulting pattern.
 		text += chars[patterns.index(pattern)]
 
+# Output the resulting string to console.
 print text
 
+# Write the string to a file.
 out = open('haymarket.txt', 'a')
 out.write(text)
